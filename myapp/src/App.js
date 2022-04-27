34d46dee4c7ecd9c1c6a0d1651535e47cc2e6b27
Nagendra Muralidhar Pulla <93373775+nag-murali@users.noreply.github.com>
2022-04-27 13:34:55 +0530

    redux saga

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Counter} from "./components/counter";
+import {Mall} from "./components/products"
+function App() {
+  return (
+    <div className="App">
+     <h1>gfdgfgf</h1>
+     <Counter/>
+     <Mall/>
+    </div>
+  );
+}
+
+export default App;
